ecfd7aad66908dab272cfa71511fb30481b64cc5
toanpham-99 <toanpham1506@gmail.com>
2021-09-05 22:45:36 +0700

    firs commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Navbar from "./component/Navbar/Navbar";
+import TourList from "./component/TourList";
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <TourList />
+    </div>
+  );
+}
+
+export default App;
